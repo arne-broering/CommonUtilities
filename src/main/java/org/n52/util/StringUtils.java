9b6ac8235dd0f52495db15882f2250238da7dffc
arne-broering <arneb@uni-muenster.de>
2013-05-02 16:48:28 +0200

    new pretty print XML method

--- src/main/java/org/n52/util/StringUtils.java
+++ src/main/java/org/n52/util/StringUtils.java
@@ -1,4 +1,16 @@
 package org.n52.util;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
 /**
  * @author <a href="mailto:broering@52north.org">Arne Broering</a>
  */
@@ -23,6 +35,26 @@ public class StringUtils {
         }
         
         return baseString;
+    }
+    
+    /**
+     * Takes an InputStream containing a String and converts it to pretty XML using the specified indent.
+     * 
+     * @param in
+     * @param indent
+     * @return
+     * @throws TransformerException
+     */
+    public static String prettyFormatAsXML(InputStream in, int indent) throws TransformerException {
+        Source xmlInput = new StreamSource(in);
+        StringWriter stringWriter = new StringWriter();
+        StreamResult xmlOutput = new StreamResult(stringWriter);
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        transformerFactory.setAttribute("indent-number", indent);
+        Transformer transformer = transformerFactory.newTransformer(); 
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(xmlInput, xmlOutput);
+        return xmlOutput.getWriter().toString();
     }
     
 }
